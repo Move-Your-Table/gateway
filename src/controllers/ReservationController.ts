92bde664a1c7279257792d5a0f18605e45f6df79
Bo <bo.robbrecht@student.howest.be>
2021-12-06 16:07:28 +0100

    Added endpoint to edit a reservation

--- src/controllers/ReservationController.ts
+++ src/controllers/ReservationController.ts
@@ -1,9 +1,10 @@
 import { Controller } from "@tsed/di";
-import { BodyParams } from "@tsed/platform-params";
-import { Description, Get, Post, Returns, Summary, Tags } from "@tsed/schema";
+import { BodyParams, PathParams } from "@tsed/platform-params";
+import { Description, Get, Patch, Post, Returns, Summary, Tags } from "@tsed/schema";
 import { Docs } from "@tsed/swagger";
 import Reservation from "src/models/Reservation/Reservation";
 import ReservationConstructor from "src/models/Reservation/ReservationConstructor";
+import ReservationMutator from "src/models/Reservation/ReservationMutator";
 
 @Controller("/reservations")
 @Docs("admin-api", "general-api")
@@ -11,7 +12,7 @@ import ReservationConstructor from "src/models/Reservation/ReservationConstructo
 export class ReservationController {
   @Post("/")
   @Summary("Make a reservation for a 🔑-identified desk/room")
-  @Description("- When the deskId is undefined, null or not given, the room will be reserved. \n - For date, you can use a EMCAScript Date object and parse this to a string")
+  @Description("- When the desk-Id is undefined, null or not given, the room will be reserved. \n - For date, you can use a ECMAScript Date object and parse this to a string, we'll handle the rest.")
   @Returns(200, Reservation)
   @Returns(400).Description("Bad Request")
   @Returns(403).Description("Not Found")
@@ -31,4 +32,30 @@ export class ReservationController {
       }
     }
   }
+
+  @Patch("/:reservationId")
+  @Summary("Edit a 🔑-identified reservation")
+  @Description("- When the desk-Id is undefined, null or not given, the room will be reserved. \n - For date, you can use a ECMAScript Date object and parse this to a string, we'll handle the rest.")
+  @Returns(200, Reservation)
+  @Returns(400).Description("Bad Request")
+  @Returns(403).Description("Not Found")
+  editReservation(@BodyParams() payload: ReservationMutator, @PathParams("reservationId") rId:number): Reservation {
+    return {
+      id: rId,
+      buildingId: payload.buildingId || 0,
+      roomId: payload.roomId || 0,
+      deskId: payload.deskId || 0,
+      startTime: new Date(payload.startTime) || new Date(),
+      endTime: new Date(payload.endTime) || new Date(),
+      reserved_for: {
+        id: 1,
+        first_name: "JJ",
+        last_name: "Johnson",
+        company: "NB Electronics"
+      }
+    }
+  }
 }
+
+  
+
