cbc9ca4f5337977aa6ff9faaf57ae2aba26d1763
Bo <bo.robbrecht@student.howest.be>
2021-12-06 16:27:04 +0100

    Added endpoint to retrieve reservations of a user

--- src/controllers/ReservationController.ts
+++ src/controllers/ReservationController.ts
@@ -1,6 +1,6 @@
 import { Controller } from "@tsed/di";
-import { BodyParams, PathParams } from "@tsed/platform-params";
-import { Description, Get, Patch, Post, Returns, Summary, Tags } from "@tsed/schema";
+import { BodyParams, PathParams, QueryParams } from "@tsed/platform-params";
+import { Delete, Description, Get, Minimum, Patch, Post, Required, Returns, Summary, Tags } from "@tsed/schema";
 import { Docs } from "@tsed/swagger";
 import Reservation from "src/models/Reservation/Reservation";
 import ReservationConstructor from "src/models/Reservation/ReservationConstructor";
@@ -10,12 +10,37 @@ import ReservationMutator from "src/models/Reservation/ReservationMutator";
 @Docs("admin-api", "general-api")
 @Tags("Reservations")
 export class ReservationController {
+  @Get("/")
+  @Summary("Get all reservations of a user")
+  @Returns(200, Array).Of(Reservation)
+  getReservations(@QueryParams("userId") @Required() @Minimum(0) id: number): Array<Reservation> {
+    const json: Array<Reservation> = [];
+    for (let i = 0; i < 10; i++) {
+      const element = {
+        id: i + 1,
+        buildingId: i + 2,
+        roomId: i + 3,
+        deskId: i + 4,
+        startTime: new Date(),
+        endTime: new Date(),
+        reserved_for: {
+          id: id,
+          first_name: "JJ",
+          last_name: "Johnson",
+          company: "NB Electronics"
+        }
+      }
+      json.push(element);
+    }
+    return json.filter(item => item.reserved_for.id === id);
+  }
+
   @Post("/")
   @Summary("Make a reservation for a 🔑-identified desk/room")
   @Description("- When the desk-Id is undefined, null or not given, the room will be reserved. \n - For date, you can use a ECMAScript Date object and parse this to a string, we'll handle the rest.")
   @Returns(200, Reservation)
   @Returns(400).Description("Bad Request")
-  @Returns(403).Description("Not Found")
+  @Returns(404).Description("Not Found")
   createReservation(@BodyParams() payload: ReservationConstructor): Reservation {
     return {
       id: Math.floor(200),
@@ -34,12 +59,12 @@ export class ReservationController {
   }
 
   @Patch("/:reservationId")
-  @Summary("Edit a 🔑-identified reservation")
+  @Summary("Edit a 🔑-identified reservation 🥽")
   @Description("- When the desk-Id is undefined, null or not given, the room will be reserved. \n - For date, you can use a ECMAScript Date object and parse this to a string, we'll handle the rest.")
   @Returns(200, Reservation)
   @Returns(400).Description("Bad Request")
-  @Returns(403).Description("Not Found")
-  editReservation(@BodyParams() payload: ReservationMutator, @PathParams("reservationId") rId:number): Reservation {
+  @Returns(404).Description("Not Found")
+  editReservation(@BodyParams() payload: ReservationMutator, @PathParams("reservationId") rId: number): Reservation {
     return {
       id: rId,
       buildingId: payload.buildingId || 0,
@@ -55,7 +80,25 @@ export class ReservationController {
       }
     }
   }
+
+  @Delete("/:reservationId")
+  @Summary("Delete a 🔑-identified reservation 🧨")
+  @Returns(200, Reservation)
+  @Returns(404).Description("Not Found")
+  deleteReservation(): Reservation {
+    return {
+      id: Math.floor(200),
+      buildingId: 1,
+      roomId: 2,
+      deskId: 6,
+      startTime: new Date(),
+      endTime: new Date(),
+      reserved_for: {
+        id: 1,
+        first_name: "JJ",
+        last_name: "Johnson",
+        company: "NB Electronics"
+      }
+    }
+  }
 }
-
-  
-
